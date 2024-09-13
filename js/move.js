e8ab7acf4c1503d15126dd679dbd8bc7ca8ff9ad
ikumisato <kinoko7971@gmail.com>
2024-09-13 15:03:00 +0900

    整理

--- js/move.js
+++ js/move.js
@@ -48,3 +48,15 @@ setTimeout(() => {
 }, 1500);
 
 // ↑↑↑ レビュー ↑↑↑
+
+// ページトップボタンの要素を取得
+const pageTopButton = document.getElementById("js-page-top");
+
+// クリックイベントをアロー関数で設定
+pageTopButton.addEventListener("click", () => {
+  // ページの一番上へスムーズにスクロール
+  window.scrollTo({
+    top: 0,
+    behavior: "smooth",
+  });
+});
